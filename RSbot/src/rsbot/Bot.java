b872671267d0097fe81582f9f4ce3a2d1b9b0f20
madilynsimons <madilynsimons@gmail.com>
2017-02-18 13:11:15 -0500

    lets run to the rock

--- RSbot/src/rsbot/Bot.java
+++ RSbot/src/rsbot/Bot.java
@@ -41,14 +41,26 @@ public class Bot {
     public void execute(){
         robot.setAutoDelay(40);
         robot.setAutoWaitForIdle(true);
+        
+        //running to the rock
+        robot.mouseMove(1316, 60);
+        leftClick();
+        robot.mouseMove(1902, 248);
+        leftClick();
+        robot.mouseMove(1868, 372);
+        leftClick();
+        robot.mouseMove(1806, 407);
+        leftClick();
+        robot.mouseMove(806, 819);
+        leftClick();
     }
     
      private void leftClick()
     {
-    robot.mousePress(InputEvent.BUTTON1_MASK);
-    robot.delay(200);
-    robot.mouseRelease(InputEvent.BUTTON1_MASK);
-    robot.delay(200);
+     robot.mousePress(InputEvent.BUTTON1_MASK);
+     robot.delay(200);
+     robot.mouseRelease(InputEvent.BUTTON1_MASK);
+     robot.delay(200);
      }
   
      private void type(int i)
