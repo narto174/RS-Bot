ae9838cdbc5dc95163b4269a35dec3a98c145e7a
madilynsimons <madilynsimons@gmail.com>
2017-02-18 12:50:19 -0500

    RS bot

---
+++ RSbot/src/rsbot/Bot.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsbot;
+
+/**
+ *
+ * @author madilynsimons
+ */
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.*;
+import java.io.*;
+
+public class Bot {
+    
+    int x1, x2, y1, y2;
+    Robot robot = new Robot();
+    
+    public Bot() throws AWTException{
+        x1 = 0;
+        x2 = 29;
+        y1 = 1917;
+        y2 = 1030;
+    }
+    
+    public Bot(int x1, int x2, int y1, int y2) throws AWTException{
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+    
+    /*
+    * run this method once the bot has been constructed
+    */
+    public void execute(){
+        robot.setAutoDelay(40);
+        robot.setAutoWaitForIdle(true);
+    }
+    
+     private void leftClick()
+    {
+    robot.mousePress(InputEvent.BUTTON1_MASK);
+    robot.delay(200);
+    robot.mouseRelease(InputEvent.BUTTON1_MASK);
+    robot.delay(200);
+     }
+  
+     private void type(int i)
+     {
+    robot.delay(40);
+    robot.keyPress(i);
+    robot.keyRelease(i);
+     }
+
+  private void type(String s)
+  {
+    byte[] bytes = s.getBytes();
+    for (byte b : bytes)
+    {
+      int code = b;
+      // keycode only handles [A-Z] (which is ASCII decimal [65-90])
+      if (code > 96 && code < 123) code = code - 32;
+      robot.delay(40);
+      robot.keyPress(code);
+      robot.keyRelease(code);
+    }
+  }
+    
+}
